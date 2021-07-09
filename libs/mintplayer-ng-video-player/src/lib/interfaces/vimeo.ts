2ba29e15aa6d968ff9e2f100040e4023ef2ff76d
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2021-07-09 09:50:08 +0200

    Temp commit

---
+++ libs/mintplayer-ng-video-player/src/lib/interfaces/vimeo.ts
@@ -0,0 +1,84 @@
+declare namespace Vimeo {
+    export class Player {
+        constructor(name: string, options: Partial<VimeoPlayerOptions>);
+
+        ready(): Promise<boolean>;
+        destroy(): Promise<boolean>;
+
+        play(): void;
+        pause(): void;
+        getPaused(): boolean;
+
+        getVideoId(): string;
+        loadVideo(id: string): Promise<any>;
+
+        getFullscreen(): boolean;
+        requestFullscreen(): Promise<boolean>;
+        exitFullscreen(): Promise<boolean>;
+
+        getPictureInPicture(): boolean;
+        requestPictureInPicture(): Promise<any>;
+        exitPictureInPicture(): Promise<any>;
+
+        getVolume(): number;
+        setVolume(volume: number): void;
+
+        getTextTracks(): any[];
+        enableTextTrack(): void;
+        disableTextTrack(): void;
+
+        getAutopause(): boolean;
+        setAutopause(autopause: boolean): void;
+
+        getBuffered(): boolean;
+
+        getCurrentTime(): number;
+        setCurrentTime(time: number): void;
+
+        getDuration(): number;
+        getEnded(): boolean;
+        
+        getLoop(): boolean;
+        setLoop(loop: boolean): void;
+
+        getMuted(): boolean;
+        setMuted(muted: boolean): void;
+
+        on(e: PlayerEvents, t: (...parms: any[]) => void): void;
+        off(e: PlayerEvents, t: (...parms: any[]) => void): void;
+    }
+
+    export type PlayerEvents = 'fullscreenchange'
+                             | 'enterpictureinpicture'
+                             | 'leavepictureinpicture'
+                             | 'volumechange';
+
+    export interface VimeoPlayerOptions {
+        /** Required. Either the id or the url of the video. */
+        id: number | string;
+
+        /** Automatically start playback of the video. Note that this won’t work on some devices. */
+        autoplay: boolean;
+
+        /** The exact width of the video. Defaults to the width of the largest available version of the video. */
+        width: number;
+
+        /** The exact height of the video. Defaults to the height of the largest available version of the video. */
+        height: number;
+
+        /** Play the video again when it reaches the end. */
+        loop: boolean;
+
+        /** Mute this video on load. Required to autoplay in certain browsers. */
+        muted: boolean;
+
+        /** 	Show the picture-in-picture button in the controlbar and enable the picture-in-picture API. */
+        pip: boolean;
+
+        /** Play video inline on mobile devices, to automatically go fullscreen on playback set this parameter to false. */
+        playsinline: boolean;
+
+        /** Show the title on the video. */
+        title: boolean;
+    }
+}
