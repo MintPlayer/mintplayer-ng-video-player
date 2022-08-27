5841d842df8320874c363327b6a8a52834e244e7
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2022-08-27 19:03:19 +0200

    Update app

--- apps/video-player-demo/src/app/app.module.ts
+++ apps/video-player-demo/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsNavbarModule } from '@mintplayer/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, BsNavbarModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
