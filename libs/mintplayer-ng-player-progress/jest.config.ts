6d8b1133fe2a136d844dfcaca2020ec7c61afb35
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-05-08 09:25:15 +0200

    nx migrate

--- libs/mintplayer-ng-player-progress/jest.config.ts
+++ libs/mintplayer-ng-player-progress/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'mintplayer-ng-player-progress',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../coverage/libs/mintplayer-ng-player-progress',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
