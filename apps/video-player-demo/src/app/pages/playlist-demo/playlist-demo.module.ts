d1ebf78f46e18a08585a6c321fa3222fa11180dd
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-06-20 00:28:33 +0200

    @nx/jest:jest - Load YoutubeModule

--- apps/video-player-demo/src/app/pages/playlist-demo/playlist-demo.module.ts
+++ apps/video-player-demo/src/app/pages/playlist-demo/playlist-demo.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BsGridModule } from '@mintplayer/ng-bootstrap/grid';
+import { BsIconModule } from '@mintplayer/ng-bootstrap/icon';
 import { BsSelectModule } from '@mintplayer/ng-bootstrap/select';
 import { BsListGroupModule } from '@mintplayer/ng-bootstrap/list-group';
+import { BsButtonTypeModule } from '@mintplayer/ng-bootstrap/button-type';
 import { BsToggleButtonModule } from '@mintplayer/ng-bootstrap/toggle-button';
-import { VideoPlayerModule } from '@mintplayer/ng-video-player';
+import { YoutubePlayerModule } from '@mintplayer/ng-youtube-player';
 import { PlaylistControllerModule } from '@mintplayer/ng-playlist-controller';
 
 import { PlaylistDemoRoutingModule } from './playlist-demo-routing.module';
 import { PlaylistDemoComponent } from './playlist-demo.component';
-import { BsButtonTypeModule } from '@mintplayer/ng-bootstrap/button-type';
 
 
 @NgModule({
@@ -21,11 +22,12 @@ import { BsButtonTypeModule } from '@mintplayer/ng-bootstrap/button-type';
     CommonModule,
     FormsModule,
     BsGridModule,
+    BsIconModule,
     BsSelectModule,
     BsListGroupModule,
     BsButtonTypeModule,
     BsToggleButtonModule,
-    VideoPlayerModule,
+    YoutubePlayerModule,
     PlaylistControllerModule,
     PlaylistDemoRoutingModule
   ]
