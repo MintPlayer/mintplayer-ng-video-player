abeed301b6125d1c785e91c4f19cd2ee9fa159f2
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-11-12 15:06:37 +0100

    Move demo page to AppComponent to prove that the services aren't lazy loaded

--- apps/video-player-demo/src/app/app-routing.module.ts
+++ apps/video-player-demo/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-    { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }
 ];
 
 @NgModule({
--- apps/video-player-demo/src/app/app.module.ts
+++ apps/video-player-demo/src/app/app.module.ts
@@ -1,17 +1,65 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsNavbarModule } from '@mintplayer/ng-bootstrap/navbar';
-import ngVideoPlayerJson from '@mintplayer/ng-video-player/package.json';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BsFormModule } from '@mintplayer/ng-bootstrap/form';
+import { BsGridModule } from '@mintplayer/ng-bootstrap/grid';
+import { BsRangeModule } from '@mintplayer/ng-bootstrap/range';
+import { BsListGroupModule } from '@mintplayer/ng-bootstrap/list-group';
+import { BsInputGroupModule } from '@mintplayer/ng-bootstrap/input-group';
+import { BsButtonTypeModule } from '@mintplayer/ng-bootstrap/button-type';
+import { BsButtonGroupModule } from '@mintplayer/ng-bootstrap/button-group';
+import { BsToggleButtonModule } from '@mintplayer/ng-bootstrap/toggle-button';
+import { BsAlertModule } from '@mintplayer/ng-bootstrap/alert';
+import { YoutubePlayerModule } from '@mintplayer/ng-youtube-player';
+import { DailymotionPlayerModule } from '@mintplayer/ng-dailymotion-player';
+import { VimeoPlayerModule } from '@mintplayer/ng-vimeo-player';
+import { SoundcloudPlayerModule } from '@mintplayer/ng-soundcloud-player';
+import { SpotifyPlayerModule } from '@mintplayer/ng-spotify-player';
+import { TwitchPlayerModule } from '@mintplayer/ng-twitch-player';
+import { MixcloudPlayerModule } from '@mintplayer/ng-mixcloud-player';
+import { FacebookPlayerModule } from '@mintplayer/ng-facebook-player';
+import { VidyardPlayerModule } from '@mintplayer/ng-vidyard-player';
+import { WistiaPlayerModule } from '@mintplayer/ng-wistia-player';
+import { StreamablePlayerModule } from '@mintplayer/ng-streamable-player';
+import { FilePlayerModule } from '@mintplayer/ng-file-player';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, BsNavbarModule, AppRoutingModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    BsFormModule,
+    BsGridModule,
+    BsRangeModule,
+    BsListGroupModule,
+    BsInputGroupModule,
+    BsButtonTypeModule,
+    BsButtonGroupModule,
+    BsToggleButtonModule,
+    BsAlertModule,
+    
+    YoutubePlayerModule,
+    DailymotionPlayerModule,
+    VimeoPlayerModule,
+    SoundcloudPlayerModule,
+    SpotifyPlayerModule,
+    TwitchPlayerModule,
+    MixcloudPlayerModule,
+    FacebookPlayerModule,
+    VidyardPlayerModule,
+    WistiaPlayerModule,
+    StreamablePlayerModule,
+    FilePlayerModule,
+    AppRoutingModule
+  ],
   providers: [
-    { provide: 'VIDEO_PLAYER_VERSION', useValue: ngVideoPlayerJson.version }
   ],
   bootstrap: [AppComponent],
 })
