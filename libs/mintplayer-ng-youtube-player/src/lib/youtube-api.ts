78ccf4596d8eeb95f7770c870ce9e9516d28b10b
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-11-12 19:51:41 +0100

    Temp commit

--- libs/mintplayer-ng-youtube-player/src/lib/youtube-api.ts
+++ libs/mintplayer-ng-youtube-player/src/lib/youtube-api.ts
@@ -1,8 +1,3 @@
-console.warn('hi there 2');
-
 import { loadApi } from "@mintplayer/ng-video-player";
 
-(function() {
-    console.warn('hi there');
-    loadApi(() => import('@mintplayer/ng-youtube-player/service').then(m => new m.YoutubeApiService()));
-})();
+loadApi(() => import('@mintplayer/ng-youtube-player/service').then(m => new m.YoutubeApiService()));
