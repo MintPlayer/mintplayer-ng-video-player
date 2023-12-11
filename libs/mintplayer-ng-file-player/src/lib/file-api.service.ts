bc46382490a103b22661d03e404de713041466a0
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-12-11 14:34:12 +0100

    Audio equalizer

--- libs/mintplayer-ng-file-player/src/lib/file-api.service.ts
+++ libs/mintplayer-ng-file-player/src/lib/file-api.service.ts
@@ -1,8 +1,8 @@
-import { DestroyRef, Inject, Injectable, PLATFORM_ID } from "@angular/core";
+import { DestroyRef, EventEmitter, Inject, Injectable, PLATFORM_ID } from "@angular/core";
 import { DOCUMENT, isPlatformServer } from "@angular/common";
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ECapability, EPlayerState, IApiService, PlayerAdapter, PlayerOptions, PrepareHtmlOptions, createPlayerAdapter } from "@mintplayer/ng-player-provider";
-import { Subject, fromEvent, take, takeUntil, timer } from 'rxjs';
+import { BehaviorSubject, Subject, filter, fromEvent, take, takeUntil, timer } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -52,12 +52,20 @@ export class FileApiService implements IApiService {
         if (!options.initialVideoId) {
             throw 'The Facebook api requires an initial video id';
         }
-  
+
         const info: MediaType = JSON.parse(options.initialVideoId);
         const id = info.id.replace(/["<>]/, '');
         switch (info.tagType) {
             case 'audio':
-                return `<audio src="${id}" autoplay="${options.autoplay ? 'on' : 'off'}" controls="on"></audio>`
+                return `
+                    <div style="width:${options.width}px;height:${options.height}px;position:relative;margin:0 auto">
+                        <div style="height:100%;display:flex;width:100%;flex-direction:column;align-items:center;justify-content:space-around;position:absolute">
+                            <audio src="${id}" autoplay="${options.autoplay ? 'on' : 'off'}" controls="on" style="z-index:5"></audio>
+                        </div>
+                        <div style="height:100%;display:flex;width:100%;flex-direction:column;align-items:center;justify-content:space-around;position:absolute">
+                            <canvas width="${options.width}" height="${options.height}"></canvas>
+                        </div>
+                    </div>`
             case 'video':
                 return `<video src="${id}" style="max-width:100%" autoplay="${options.autoplay ? 'on' : 'off'}" width="${options.width ?? 500}" height="${options.height ?? 300}" controls></video>`
             default:
@@ -75,6 +83,8 @@ export class FileApiService implements IApiService {
             if (!mediaElement) {
                 return rejectPlayer('There doesn\'t seem to be an audio or video element');
             }
+
+            const divElement = options.element.querySelector('div')!;
 
             const destroyRef = new Subject();
             let adapter: PlayerAdapter;
@@ -110,11 +120,9 @@ export class FileApiService implements IApiService {
                     }),
                     setFullscreen: (isFullscreen) => {
                         if (isFullscreen) {
-                            mediaElement.requestFullscreen({
-                                // navigationUI: 'hide'
-                            });
+                            divElement.requestFullscreen();
                         } else {
-                            if (this.document.fullscreenElement === mediaElement) {
+                            if (this.document.fullscreenElement === divElement) {
                                 this.document.exitFullscreen();
                             }
                         }
@@ -143,6 +151,13 @@ export class FileApiService implements IApiService {
                     }
                 });
 
+                if (mediaElement instanceof HTMLAudioElement) {
+                    const canvases = options.element.getElementsByTagName('canvas');
+                    if (canvases.length > 0) {
+                      this.createEqualizer(mediaElement, canvases[0], destroy);
+                    }
+                }
+
                 fromEvent(mediaElement, 'volumechange')
                     .pipe(takeUntil(destroyRef), takeUntilDestroyed(destroy))
                     .subscribe(() => {
@@ -153,27 +168,27 @@ export class FileApiService implements IApiService {
                 fromEvent(mediaElement, 'enterpictureinpicture')
                     .pipe(takeUntil(destroyRef), takeUntilDestroyed(destroy))
                     .subscribe(() => adapter.onPipChange(true));
-                    
+
                 fromEvent(mediaElement, 'leavepictureinpicture')
                     .pipe(takeUntil(destroyRef), takeUntilDestroyed(destroy))
                     .subscribe(() => adapter.onPipChange(false));
-                
-                fromEvent(mediaElement, 'fullscreenchange')
+
+                fromEvent(divElement, 'fullscreenchange')
                     .pipe(takeUntil(destroyRef), takeUntilDestroyed(destroy))
-                    .subscribe(() => adapter.onFullscreenChange(this.document.fullscreenElement === mediaElement));
+                    .subscribe(() => adapter.onFullscreenChange(this.document.fullscreenElement === divElement));
 
                 fromEvent(mediaElement, 'play')
                     .pipe(takeUntil(destroyRef), takeUntilDestroyed(destroy))
                     .subscribe(() => adapter.onStateChange(EPlayerState.playing));
-                    
+
                 fromEvent(mediaElement, 'pause')
                     .pipe(takeUntil(destroyRef), takeUntilDestroyed(destroy))
                     .subscribe(() => adapter.onStateChange(EPlayerState.paused));
-            
+
                 fromEvent(mediaElement, 'ended')
                     .pipe(takeUntil(destroyRef), takeUntilDestroyed(destroy))
                     .subscribe(() => adapter.onStateChange(EPlayerState.ended));
-                
+
                 if (!isPlatformServer(this.platformId)) {
                     timer(0, 50)
                         .pipe(takeUntil(destroyRef), takeUntilDestroyed(destroy))
@@ -192,6 +207,65 @@ export class FileApiService implements IApiService {
             });
         });
     }
+
+    private createEqualizer(audio: HTMLAudioElement, canvas: HTMLCanvasElement, destroy: DestroyRef) {
+        const audioContext = new AudioContext();
+        const analyzer = audioContext.createAnalyser();
+
+        const source = audioContext.createMediaElementSource(audio);
+        source.connect(analyzer);
+        analyzer.connect(audioContext.destination);
+        this.frameLooper(analyzer, canvas, destroy);
+
+        destroy.onDestroy(() => {
+            analyzer.disconnect();
+            source.disconnect();
+            audioContext.close();
+        });
+    }
+
+    private frameLooper(analyser: AnalyserNode, canvas: HTMLCanvasElement, destroy: DestroyRef) {
+        const drawContext = canvas.getContext('2d');
+        if (!drawContext) {
+            throw 'Could not get drawing context';
+        }
+
+        let cancelled = false;
+        destroy.onDestroy(() => cancelled = true);
+
+        if (typeof window !== 'undefined') {
+            const anyWindow = <any>window;
+            const callback = (time: DOMHighResTimeStamp) => {
+                // const requestAnimationFrameCrossBrowser =
+                anyWindow.RequestAnimationFrame =
+                    window.requestAnimationFrame(callback) ||
+                    anyWindow.msRequestAnimationFrame(callback) ||
+                    anyWindow.mozRequestAnimationFrame(callback) ||
+                    anyWindow.webkitRequestAnimationFrame(callback);
+
+                const fbc_array = new Uint8Array(analyser.frequencyBinCount);
+                const bar_count = window.innerWidth / 2;
+
+                analyser.getByteFrequencyData(fbc_array);
+
+                drawContext.clearRect(0, 0, canvas.width, canvas.height);
+                drawContext.fillStyle = "#888888";
+
+                for (let i = 0; i < bar_count; i++) {
+                    const bar_pos = i * 4;
+                    const bar_width = 2;
+                    const bar_height = -(fbc_array[i] / 2);
+
+                    drawContext.fillRect(bar_pos, canvas.height, bar_width, bar_height);
+                }
+
+            }
+
+            if (!cancelled) {
+                callback(0);
+            }
+        }
+    }
 }
 
 interface MediaType {
@@ -199,3 +273,17 @@ interface MediaType {
     id: string;
     extension: string;
 }
+
+// class CancellationToken {
+//     private _isCancelled = false;
+//     public get isCancelled() {
+//         return this._isCancelled;
+//     }
+
+//     public cancel() {
+//         this._isCancelled = true;
+//         this.onCancel.emit();
+//     }
+
+//     public onCancel = new EventEmitter();
+// }
