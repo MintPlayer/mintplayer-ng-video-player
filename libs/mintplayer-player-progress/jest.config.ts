6e13f32e5ae09c7e943d791cc6778df29421941f
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-03-20 22:34:37 +0100

    Update jest.config.ts

--- libs/mintplayer-player-progress/jest.config.ts
+++ libs/mintplayer-player-progress/jest.config.ts
@@ -2,21 +2,17 @@
 export default {
   displayName: 'mintplayer-player-progress',
   preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/libs/mintplayer-player-progress',
+  globals: {},
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
       },
     ],
   },
-  // Had to add <rootDir>/ But this slows tests down tremendously
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
+  moduleFileExtensions: ['ts', 'js', 'html'],
 };
+
--- libs/mintplayer-playlist-controller/jest.config.ts
+++ libs/mintplayer-playlist-controller/jest.config.ts
@@ -2,21 +2,16 @@
 export default {
   displayName: 'mintplayer-playlist-controller',
   preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/libs/mintplayer-playlist-controller',
+  globals: {},
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
       },
     ],
   },
-  // Had to add <rootDir>/ But this slows tests down tremendously
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
+  moduleFileExtensions: ['ts', 'js', 'html'],
 };
