c969930027d59e4124c09530c81acd46e0daa709
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-03-20 23:15:04 +0100

    Regenerate library

--- libs/mintplayer-dailymotion-player/README.md
+++ libs/mintplayer-dailymotion-player/README.md
@@ -2,6 +2,10 @@
 
 This library was generated with [Nx](https://nx.dev).
 
+## Building
+
+Run `nx build mintplayer-dailymotion-player` to build the library.
+
 ## Running unit tests
 
-Run `nx test mintplayer-dailymotion-player` to execute the unit tests.
+Run `nx test mintplayer-dailymotion-player` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/mintplayer-dailymotion-player/jest.config.ts
+++ libs/mintplayer-dailymotion-player/jest.config.ts
@@ -2,16 +2,10 @@
 export default {
   displayName: 'mintplayer-dailymotion-player',
   preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/libs/mintplayer-dailymotion-player',
-  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-      },
-    ],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/mintplayer-dailymotion-player',
 };
