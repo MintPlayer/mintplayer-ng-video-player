509ea2d677399877ac6bd87416a66c2cb4c67674
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-07-22 21:11:49 +0200

    Move projects

---
+++ libs/frameworks/mintplayer-r-video-player/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/mintplayer-r-video-player';
---
+++ libs/frameworks/mintplayer-r-video-player/src/lib/mintplayer-r-video-player.tsx
@@ -0,0 +1,11 @@
+import styles from './mintplayer-r-video-player.module.scss';
+
+export function MintplayerRVideoPlayer() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to MintplayerRVideoPlayer!</h1>
+    </div>
+  );
+}
+
+export default MintplayerRVideoPlayer;
