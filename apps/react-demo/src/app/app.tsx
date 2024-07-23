a15ce94f9d6271dcfecb102813baf7c78aacde85
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-07-24 00:27:19 +0200

    React to input change

--- apps/react-demo/src/app/app.tsx
+++ apps/react-demo/src/app/app.tsx
@@ -2,14 +2,29 @@
 import styles from './app.module.scss';
 
 import { RVideoPlayer } from '@mintplayer/r-video-player';
+import { useState } from 'react';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
+  const [count, setCount] = useState(0);
+  const [likes, setLikes] = useState(0);
+
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const incrementLikes = () => {
+    setLikes(likes + 1);
+  };
+
   return (
     <div>
-      <RVideoPlayer title="react-demo" />
-
+      <RVideoPlayer title="react-demo" counter={count} likes={likes} />
+      <button onClick={incrementCount}>Increment Count</button>
+      <button onClick={incrementLikes}>Increment Likes</button>
+      { count } / { likes }
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
--- libs/frameworks/r-video-player/src/lib/r-video-player.tsx
+++ libs/frameworks/r-video-player/src/lib/r-video-player.tsx
@@ -1,9 +1,15 @@
+import { useEffect } from 'react';
 import styles from './r-video-player.module.scss';
 
-export function RVideoPlayer({ title }: { title: string }) {
+export function RVideoPlayer({ title, counter, likes }: { title: string, counter: number, likes: number }) {
+  useEffect(() => {
+    console.log('Counter changed', counter);
+  }, [counter]);
+
   return (
     <div className={styles['container']}>
       <h1>Welcome to {title}!</h1>
+      <h2>Counter {counter}</h2>
     </div>
   );
 }
