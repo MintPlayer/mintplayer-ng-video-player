f098a0bf8314e356937e1a5b3f53dc14ee5707c3
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-07-25 20:16:20 +0200

    Temp commit

--- apps/video-player-demo/src/app/app.component.spec.ts
+++ apps/video-player-demo/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        AppComponent,
-      ],
+      imports: [AppComponent],
       declarations: [],
       providers: []
     }).compileComponents();
