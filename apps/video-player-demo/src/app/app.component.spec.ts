35c30ed4e50bf317aad77aa7eee4c01c968d6039
Pieterjan <pieterjandeclippel@msn.com>
2024-07-30 14:42:44 +0200

    Standalone routing (#72)
    * Standalone routing
    
    * temp commit
    
    * temp commit
    
    * Use mockmodule
    
    * Fixed circular dep issue
    
    * Cleanup
    
    * Bump version

--- apps/video-player-demo/src/app/app.component.spec.ts
+++ apps/video-player-demo/src/app/app.component.spec.ts
@@ -1,39 +1,37 @@
-import { Component, ContentChildren, Directive, forwardRef, Input, QueryList } from '@angular/core';
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MockModule } from 'ng-mocks';
+import { BsNavbarModule } from '@mintplayer/ng-bootstrap/navbar';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule.withRoutes([
-          { path: 'youtube', component: YoutubeMockComponent },
-          { path: 'vimeo', component: VimeoMockComponent },
-          { path: 'soundcloud', component: SoundcloudMockComponent },
-          { path: 'video', component: VideoMockComponent },
-          { path: 'playlist', component: PlaylistMockComponent },
-        ])
+        AppComponent,
+        
+        // Mock components
+        MockModule(BsNavbarModule),
       ],
       declarations: [
-        AppComponent,
-      
         // Mock pages
         YoutubeMockComponent,
         VimeoMockComponent,
         SoundcloudMockComponent,
         VideoMockComponent,
         PlaylistMockComponent,
-        
-        // Mock components
-        BsNavbarMockComponent,
-        BsNavbarNavMockComponent,
-        BsNavbarBrandMockComponent,
-        BsNavbarDropdownMockComponent,
-        BsNavbarItemMockComponent,
-        BsNavbarContentMockDirective,
       ],
       providers: [
+        provideRouter([
+          { path: 'youtube', component: YoutubeMockComponent },
+          { path: 'vimeo', component: VimeoMockComponent },
+          { path: 'soundcloud', component: SoundcloudMockComponent },
+          { path: 'video', component: VideoMockComponent },
+          { path: 'playlist', component: PlaylistMockComponent },
+        ]),
+        provideNoopAnimations(),
         { provide: 'VIDEO_PLAYER_VERSION', useValue: '1.0.0' }
       ]
     }).compileComponents();
@@ -60,22 +58,6 @@ describe('AppComponent', () => {
     );
   });
 });
-
-type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
-
-enum Color {
-  primary = 0,
-  secondary = 1,
-  success = 2,
-  danger = 3,
-  warning = 4,
-  info = 5,
-  light = 6,
-  dark = 7,
-  body = 8,
-  white = 9,
-  transparent = 10
-}
 
 @Component({
   selector: 'youtube-mock-page',
@@ -106,73 +88,3 @@ class VideoMockComponent { }
   template: `<div>Playlist</div>`
 })
 class PlaylistMockComponent { }
-
-@Component({
-  selector: 'bs-navbar',
-  template: `
-  <nav>
-    <div>
-      <ng-content></ng-content>
-    </div>  
-  </nav>`
-})
-class BsNavbarMockComponent {
-  @Input() color?: Color;
-  @Input() breakpoint?: Breakpoint;
-}
-
-@Component({
-  selector: 'bs-navbar-nav',
-  template: `
-  <div>
-    <ul>
-      <ng-content></ng-content>
-    </ul>  
-  </div>`
-})
-class BsNavbarNavMockComponent {
-  @Input() collapse = true;
-}
-
-@Component({
-  selector: 'bs-navbar-brand',
-  template: `
-  <div>
-    <ng-content></ng-content>
-  </div>`
-})
-class BsNavbarBrandMockComponent {}
-
-@Component({
-  selector: 'bs-navbar-dropdown',
-  template: `
-  <ul>
-    <ng-content></ng-content>
-  </ul>`,
-  providers: [
-    // { provide: BsNavbarDropdownComponent, useExisting: BsNavbarDropdownMockComponent }
-  ]
-})
-class BsNavbarDropdownMockComponent {
-}
-
-@Component({
-  selector: 'bs-navbar-item',
-  template: `
-  <li>
-    <ng-content></ng-content>
-  </li>`,
-  providers: [
-    // { provide: BsNavbarItemComponent, useExisting: BsNavbarItemMockComponent }
-  ]
-})
-class BsNavbarItemMockComponent {
-  @ContentChildren(forwardRef(() => BsNavbarDropdownMockComponent)) dropdowns!: QueryList<BsNavbarDropdownMockComponent>;
-}
-
-@Directive({
-  selector: '[bsNavbarContent]'
-})
-class BsNavbarContentMockDirective {
-  @Input('bsNavbarContent') navbar!: BsNavbarMockComponent;
-}
--- apps/video-player-demo/src/app/pages/video-demo/video-demo.component.ts
+++ apps/video-player-demo/src/app/pages/video-demo/video-demo.component.ts
@@ -2,14 +2,41 @@ import { Component, Inject, ViewChild } from '@angular/core';
 import { Color } from '@mintplayer/ng-bootstrap';
 import { ECapability, EPlayerState } from '@mintplayer/player-provider';
 import { PlayerProgress } from '@mintplayer/player-progress';
-import { VideoPlayerComponent } from '@mintplayer/ng-video-player';
+import { provideVideoApis, VideoPlayerComponent } from '@mintplayer/ng-video-player';
 import { BehaviorSubject, Observable, map } from 'rxjs';
-import { APP_BASE_HREF } from '@angular/common';
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BsFormModule } from '@mintplayer/ng-bootstrap/form';
+import { BsGridModule } from '@mintplayer/ng-bootstrap/grid';
+import { BsRangeModule } from '@mintplayer/ng-bootstrap/range';
+import { BsListGroupModule } from '@mintplayer/ng-bootstrap/list-group';
+import { BsInputGroupComponent } from '@mintplayer/ng-bootstrap/input-group';
+import { BsButtonTypeDirective } from '@mintplayer/ng-bootstrap/button-type';
+import { BsButtonGroupComponent } from '@mintplayer/ng-bootstrap/button-group';
+import { BsToggleButtonModule } from '@mintplayer/ng-bootstrap/toggle-button';
+import { BsAlertModule } from '@mintplayer/ng-bootstrap/alert';
+import { youtubeLoader } from '@mintplayer/youtube-player';
+import { dailymotionLoader } from '@mintplayer/dailymotion-player';
+import { vimeoLoader } from '@mintplayer/vimeo-player';
+import { soundCloudLoader } from '@mintplayer/soundcloud-player';
+import { mixCloudLoader } from '@mintplayer/mixcloud-player';
+import { twitchLoader } from '@mintplayer/twitch-player';
+import { spotifyLoader } from '@mintplayer/spotify-player';
+import { streamableLoader } from '@mintplayer/streamable-player';
+import { facebookLoader } from '@mintplayer/facebook-player';
+import { fileLoader } from '@mintplayer/file-player';
+import { vidyardLoader } from '@mintplayer/vidyard-player';
+import { wistiaLoader } from '@mintplayer/wistia-player';
 
 @Component({
   selector: 'mintplayer-ng-video-player-video-demo',
   templateUrl: './video-demo.component.html',
-  styleUrls: ['./video-demo.component.scss']
+  styleUrls: ['./video-demo.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, VideoPlayerComponent, BsFormModule, BsGridModule, BsRangeModule, BsListGroupModule, BsInputGroupComponent, BsButtonTypeDirective, BsButtonGroupComponent, BsToggleButtonModule, BsAlertModule],
+  providers: [
+    provideVideoApis(youtubeLoader, dailymotionLoader, vimeoLoader, soundCloudLoader, mixCloudLoader, twitchLoader, spotifyLoader, streamableLoader, facebookLoader, fileLoader, vidyardLoader, wistiaLoader)
+  ]
 })
 export class VideoDemoComponent {
 
