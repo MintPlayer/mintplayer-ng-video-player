203717bce578d1d7bcd0657fa0aa57abdcc589a8
Pieterjan <pieterjandeclippel@msn.com>
2025-10-25 21:48:10 +0200

    fix: run vitest configs in offline environment

--- apps/vue-demo/vite.config.ts
+++ apps/vue-demo/vite.config.ts
@@ -1,42 +1,47 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import type { UserConfigFn } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
-export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/vue-demo',
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [vue(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  build: {
-    outDir: '../../dist/apps/vue-demo',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+const config: UserConfigFn = async () => {
+  const { default: vue } = await import('@vitejs/plugin-vue');
+
+  return {
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/vue-demo',
+    server: {
+      port: 4200,
+      host: 'localhost',
     },
-  },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../coverage/apps/vue-demo',
-      provider: 'v8' as const,
+    preview: {
+      port: 4300,
+      host: 'localhost',
     },
-  },
-}));
+    plugins: [vue(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    build: {
+      outDir: '../../dist/apps/vue-demo',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+    test: {
+      watch: false,
+      globals: true,
+      environment: 'jsdom',
+      include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: '../coverage/apps/vue-demo',
+        provider: 'v8' as const,
+      },
+    },
+  };
+};
+
+export default config;
